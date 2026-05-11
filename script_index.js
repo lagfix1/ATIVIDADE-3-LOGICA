e2f904e6f8200ebcefbdab56c47d47ffcebd5236
Mauricio C. Arruda <mcardosoarruda@gmail.com>
2026-05-11 10:26:52 -0300

    Atualiza a lógica do jogo de adivinhação e corrige a exibição das tentativas restantes

--- script_index.js
+++ script_index.js
@@ -1,28 +1,36 @@
 //Cria um número aleatório entre 1 e 100
-const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+let tentativas = 10;
 
-//Função para exibir as tentativas restantes
-function tentativasRestantes() {
-    for (let i = 10; i > 0; i--) {
-        console.log(`Tentativas restantes: ${i}`);
-    }
+//Função para exibir atulizar o número de tentativas restantes
+function atualizarTentativas() {
+    document.getElementById('tentativas').textContent = 'Tentativas restantes: {tentativas}';
 }
 //Função para verificar o palpite do usuário
 function verificarPalpite() {
     const palpite = parseInt(document.getElementById('palpite').value);
+    const mesnagem = document.getElementById('mensagem');
+    if(tentativas <= 0) {
+        mesnagem.textContent = 'Suas tentativas acabaram! O número era: ${numeroAleatorio}';
+        return
+    }
+    if (isNaN(palpite) || palpite < 1 || palpite > 100) {
+        mesnagem.textContent = 'Por favor, insira um número válido entre 1 e 100.';
+        return;
+    }
+    tentativas--;
+    atualizarTentativas();
+
     if (palpite === numeroAleatorio) {
-        alert('Parabéns! Você acertou o número!');
+        mensagem.textContent = 'Parabéns! Você acertou o número!';
+    } else if (tentativas === 0) {
+        mensagem.textContent = `Suas tentativas acabaram! O número era ${numeroAleatorio}`;
     } else if (palpite < numeroAleatorio) {
-        alert('Tente um número maior!');
+        mensagem.textContent = 'Tente um número maior!';
     } else {
-        alert('Tente um número menor!');
+        mensagem.textContent = 'Tente um número menor!';
     }
 }
 
-    function verificarPalpite() {
-        if (tentativasRestantes <= 10) {
-            verificarPalpite();
-        } else {
-            alert('Suas tentativas acabaram! O número era: ' + numeroAleatorio);
-        }
-    }
+
+    
